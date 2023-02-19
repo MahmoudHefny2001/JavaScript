3ba3e0372d8a4fcf996557ad12fa1f8aec85ad14
MahmoudHefny2001 <hefny4@gmail.com>
2023-02-19 21:17:07 +0200

    Message

---
+++ variables.js
@@ -0,0 +1,22 @@
+const variable = 'Hello World!';
+// console.log(variable);
+
+let newVariable;
+newVariable = 60;
+// console.log(newVariable)
+
+const var1 = 10;
+let var2 = 10;
+var var3 = 10;
+
+// console.log(var1, var2, var3);
+
+const stringValue = 'Hello World';
+const anotherStringValue = "Hello World";
+
+const firstArray = [10, 20, 30, 40];
+const secondArray = [10, 'a string', {key: 'value'}, [1, 2], true];
+
+console.log(firstArray[0]);
+console.log(secondArray[3]);
+console.log(secondArray[3][0]);
